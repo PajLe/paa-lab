7cbf6e4b1390b40b8b7266ab567f5f339a9cc95f
Pavle Aleksov <pajle.ok@elfak.rs>
2020-04-12 16:17:46 +0200

    implemented RabinKarp - Search All occurrences

--- Lab vežba 1/LV1/LV1/RabinKarp.cs
+++ Lab vežba 1/LV1/LV1/RabinKarp.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -59,6 +61,51 @@ namespace LV1
             }
 
             return n;
+        }
+
+        public void SearchAll(string text, string writeToFile)
+        {
+            if (string.IsNullOrEmpty(text))
+                throw new ArgumentNullException(/**/);
+
+            int n = text.Length;
+            if (n < m) return;
+
+            using (StreamWriter sw = new StreamWriter(new FileStream(writeToFile, FileMode.Create)))
+            {
+                Stopwatch st = new Stopwatch();
+                st.Start();
+                Console.WriteLine("Start RK - " + st.Elapsed);
+                
+                long t = hash(text, m);
+                if (t == p && text.Substring(0, m).Equals(_pattern))
+                {
+                    sw.Write(st.Elapsed + " : ");
+                    sw.Write('0');
+                    sw.WriteLine(" : " + text.Substring(0, (m + 5 < n) ? m + 5 : n));
+                }
+
+                for (int i = m; i < n; i++)
+                {
+                    /*
+                 ts+1 = 31415 -   3  * 10000 =   1415 * 10 + text[i] = 14156 
+                     */
+                    t = ((t - ((text[i - m] * h) % q)) * R + text[i]) % q;
+                    if (t == p && text.Substring(i - m + 1, m).Equals(_pattern))
+                    {
+                        sw.Write(st.Elapsed + " : ");
+                        sw.Write(i - m + 1);
+                        int val = 0;
+                        int im5 = i - m - 5;
+                        if (im5 > -1) val = 5;
+                        else { im5 = 0; val = i - m; }
+                        sw.WriteLine(" : " + text.Substring(im5, (im5 + m + 10 < n) ? m + 10 : m + val + n - i));
+                    }
+                        
+                }
+                Console.WriteLine("Stop RK - " + st.Elapsed);
+                st.Stop();
+            }
         }
     }
 }
