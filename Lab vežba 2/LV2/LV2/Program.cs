555eb1402705483ee64b25efad2bf51c0c2aea77
Pavle Aleksov <pajle.ok@elfak.rs>
2020-04-17 02:31:26 +0200

    implemented Bubble Sort

--- Lab vežba 2/LV2/LV2/Program.cs
+++ Lab vežba 2/LV2/LV2/Program.cs
@@ -11,15 +11,19 @@ namespace LV2
         static void Main(string[] args)
         {
             string baseSourceRoute = "../../../../testfiles/";
-            string baseWriteRoute = "../../../../results/";
+            string baseWriteRoute = "../../../../results/"; // radix, heap, bubble
 
+
+            /*
             FileRepo.GenerateNumberFile(baseSourceRoute + "100.txt", 100);
             FileRepo.GenerateNumberFile(baseSourceRoute + "1k.txt", 1000);
             FileRepo.GenerateNumberFile(baseSourceRoute + "10k.txt", 10000);
             FileRepo.GenerateNumberFile(baseSourceRoute + "100k.txt", 100000);
             FileRepo.GenerateNumberFile(baseSourceRoute + "1m.txt", 1000000);
             FileRepo.GenerateNumberFile(baseSourceRoute + "10m.txt", 10000000);
-            FileRepo.GenerateNumberFile(baseSourceRoute + "100m.txt", 100000000);
+            FileRepo.GenerateNumberFile(baseSourceRoute + "100m.txt", 100000000);*/
+
+            FileRepo.WriteNumbersToFile(BubbleSort.Sort(FileRepo.ReadNumberFile(baseSourceRoute + "100.txt")), "out.txt");
         }
     }
 }
