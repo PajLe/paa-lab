82d648f87827411c70ea7ea9ca4ea97ab21d830d
Pavle Aleksov <pajle.ok@elfak.rs>
2020-04-17 02:28:50 +0200

    added write nums to file in FileRepo

--- Lab vežba 2/LV2/LV2/FileRepo.cs
+++ Lab vežba 2/LV2/LV2/FileRepo.cs
@@ -24,12 +24,22 @@ namespace LV2
             int[] numbers;
             using (StreamReader sr = new StreamReader(new FileStream(fileName, FileMode.Open)))
             {
-                string[] numbersAsStrings = sr.ReadToEnd().Split(' ', '\n', '\r');
+                char[] delims = { ' ', '\n', '\r' };
+                string[] numbersAsStrings = sr.ReadToEnd().Split(delims, StringSplitOptions.RemoveEmptyEntries);
                 numbers = new int[numbersAsStrings.Length];
                 for (int i = 0; i < numbers.Length; i++)
                     numbers[i] = int.Parse(numbersAsStrings[i]);
             }
             return numbers;
+        }
+
+        public static void WriteNumbersToFile(int[] numbers, string fileName)
+        {
+            using (StreamWriter sw = new StreamWriter(new FileStream(fileName, FileMode.Create)))
+            {
+                foreach (int num in numbers)
+                    sw.WriteLine(num);
+            }
         }
     }
 }
