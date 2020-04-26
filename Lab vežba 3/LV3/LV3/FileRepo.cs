0eba1aaec67fc0c1adc734a3671b33d67f2845b2
Pavle Aleksov <pajle.ok@elfak.rs>
2020-04-26 03:27:45 +0200

    initialized; added test files

---
+++ Lab vežba 3/LV3/LV3/FileRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV3
+{
+    class FileRepo
+    {
+
+        public static void GenerateAsciiFile(string fileName, int characterCount)
+        {
+            Random r = new Random();
+            using (StreamWriter sw = new StreamWriter(new FileStream(fileName, FileMode.Create)))
+            {
+                for (int j = 0; j < characterCount; j++)
+                {
+                    char c = (char)r.Next(0, 255);
+                    sw.Write(c);
+                }
+            }
+        }
+    }
+}
---
+++ Lab vežba 3/LV3/LV3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseSourceRoute = "../../../../testfiles/";
+            string baseWriteRoute = "../../../../results/";
+
+            /*FileRepo.GenerateAsciiFile("random-100.txt", 100);
+            FileRepo.GenerateAsciiFile("random-1k.txt", 1000);
+            FileRepo.GenerateAsciiFile("random-10k.txt", 10000);
+            FileRepo.GenerateAsciiFile("random-100k.txt", 100000);
+            FileRepo.GenerateAsciiFile("random-1m.txt", 1000000);*/
+        }
+    }
+}
