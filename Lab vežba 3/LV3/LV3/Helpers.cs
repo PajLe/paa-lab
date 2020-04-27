d5464d802927d12e7ae4d1b9395a9470a8a4bbea
Pavle Aleksov <pajle.ok@elfak.rs>
2020-04-27 17:41:04 +0200

    added java style implementation for TST with longest prefix operation

--- Lab vežba 3/LV3/LV3/Helpers.cs
+++ Lab vežba 3/LV3/LV3/Helpers.cs
@@ -117,4 +117,116 @@ namespace LV3
             return b;
         }
     }
+
+    /*
+     * Java style code
+     */
+    internal class Node<Value>
+    {
+        public char c;                        // character
+        public Node<Value> left, mid, right;  // left, middle, and right subtries
+        public Value val;                     // value associated with string
+    }
+
+    internal class TST<Value>
+    {
+        private int n;              // size
+        private Node<Value> root;   // root of TST
+
+
+        /**
+         * Initializes an empty string symbol table.
+         */
+        public TST()
+        {
+        }
+
+        public int size()
+        {
+            return n;
+        }
+
+        public String longestPrefixOf(String query)
+        {
+            if (query == null)
+                throw new ArgumentNullException("calls longestPrefixOf() with null argument");
+            
+            if (query.Length == 0) return null;
+            int length = 0;
+            Node<Value> x = root;
+            int i = 0;
+            while (x != null && i < query.Length)
+            {
+                char c = query[i];
+                if (c < x.c) x = x.left;
+                else if (c > x.c) x = x.right;
+                else
+                {
+                    i++;
+                    if (x.val != null) length = i;
+                    x = x.mid;
+                }
+            }
+            return query.Substring(0, length);
+        }
+
+        public void put(String key, Value val)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("calls put() with null key");
+            }
+            if (!contains(key)) n++;
+            else if (val == null) n--;       // delete existing key
+            root = put(root, key, val, 0);
+        }
+
+        private Node<Value> put(Node<Value> x, String key, Value val, int d)
+        {
+            char c = key[d];
+            if (x == null)
+            {
+                x = new Node<Value>();
+                x.c = c;
+            }
+            if (c < x.c) x.left = put(x.left, key, val, d);
+            else if (c > x.c) x.right = put(x.right, key, val, d);
+            else if (d < key.Length - 1) x.mid = put(x.mid, key, val, d + 1);
+            else x.val = val;
+            return x;
+        }
+
+        public bool contains(String key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("argument to contains() is null");
+            }
+            return get(key) != null;
+        }
+
+        public Value get(String key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("calls get() with null argument");
+            }
+            if (key.Length == 0) throw new ArgumentNullException("key must have length >= 1");
+            Node<Value> x = get(root, key, 0);
+            if (x == null) return default(Value);
+            return x.val;
+        }
+
+        // return subtrie corresponding to given key
+        private Node<Value> get(Node<Value> x, String key, int d)
+        {
+            if (x == null) return null;
+            if (key.Length == 0) throw new ArgumentNullException("key must have length >= 1");
+            char c = key[d];
+            if (c < x.c) return get(x.left, key, d);
+            else if (c > x.c) return get(x.right, key, d);
+            else if (d < key.Length - 1) return get(x.mid, key, d + 1);
+            else return x;
+        }
+    }
 }
