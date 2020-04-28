1d099561172c7c0367b338c7b39ea8a5e69dd149
Pavle Aleksov <pajle.ok@elfak.rs>
2020-04-28 03:09:21 +0200

    added tests for LZW

--- Lab vežba 3/LV3/LV3/Program.cs
+++ Lab vežba 3/LV3/LV3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,13 +16,13 @@ namespace LV3
             string baseWriteRoute = "../../../../results/";
             Stopwatch s = new Stopwatch();
 
-            /*FileRepo.GenerateAsciiFile("random-100.txt", 100);
+            FileRepo.GenerateAsciiFile("random-100.txt", 100);
             FileRepo.GenerateAsciiFile("random-1k.txt", 1000);
             FileRepo.GenerateAsciiFile("random-10k.txt", 10000);
             FileRepo.GenerateAsciiFile("random-100k.txt", 100000);
             FileRepo.GenerateAsciiFile("random-1m.txt", 1000000);*/
 
-            Console.WriteLine("sf mladic-100.txt");
+            /*Console.WriteLine("sf mladic-100.txt");
             Console.WriteLine("mladic-100.txt code table:");
             s.Start();
             ShannonFano sf_mladic100 = new ShannonFano(baseSourceRoute + "mladic-100.txt", baseWriteRoute + "sf/" + "mladic-100-sf.txt");
@@ -133,6 +134,122 @@ namespace LV3
             Console.WriteLine(s.Elapsed + "\n");
             s.Reset();
 
+            Console.WriteLine("\n\n\n______________________________________");
+            Console.Write("______________________________________\n\n\n");
+
+            Console.WriteLine("lzw mladic-100.txt");
+            Console.WriteLine("mladic-100.txt code table:");
+            s.Start();
+            LZW lzw_mladic100 = new LZW(baseSourceRoute + "mladic-100.txt", baseWriteRoute + "lzw/" + "mladic-100-lzw.txt");
+            lzw_mladic100.Decode();
+            Console.WriteLine(s.Elapsed + "\n");
+            s.Reset();
+
+            Console.WriteLine("lzw mladic-1k.txt");
+            s.Start();
+            LZW lzw_mladic1k = new LZW(baseSourceRoute + "mladic-1k.txt", baseWriteRoute + "lzw/" + "mladic-1k-lzw.txt");
+            lzw_mladic1k.Decode();
+            Console.WriteLine(s.Elapsed + "\n");
+            s.Reset();
+
+            Console.WriteLine("lzw mladic-10k.txt");
+            s.Start();
+            LZW lzw_mladic10k = new LZW(baseSourceRoute + "mladic-10k.txt", baseWriteRoute + "lzw/" + "mladic-10k-lzw.txt");
+            lzw_mladic10k.Decode();
+            Console.WriteLine(s.Elapsed + "\n");
+            s.Reset();
+
+            Console.WriteLine("lzw mladic-100k.txt");
+            s.Start();
+            LZW lzw_mladic100k = new LZW(baseSourceRoute + "mladic-100k.txt", baseWriteRoute + "lzw/" + "mladic-100k-lzw.txt");
+            lzw_mladic100k.Decode();
+            Console.WriteLine(s.Elapsed + "\n");
+            s.Reset();
+
+            Console.WriteLine("lzw mladic-1m.txt");
+            s.Start();
+            LZW lzw_mladic1m = new LZW(baseSourceRoute + "mladic-1m.txt", baseWriteRoute + "lzw/" + "mladic-1m-lzw.txt");
+            lzw_mladic1m.Decode();
+            Console.WriteLine(s.Elapsed + "\n");
+            s.Reset();
+            Console.WriteLine("______________________________________\n");
+            Console.WriteLine("______________________________________\n");
+
+            Console.WriteLine("lzw moby-100.txt");
+            Console.WriteLine("moby-100.txt code table:");
+            s.Start();
+            LZW lzw_moby100 = new LZW(baseSourceRoute + "moby-100.txt", baseWriteRoute + "lzw/" + "moby-100-lzw.txt");
+            lzw_moby100.Decode();
+            Console.WriteLine(s.Elapsed + "\n");
+            s.Reset();
+
+            Console.WriteLine("lzw moby-1k.txt");
+            s.Start();
+            LZW lzw_moby1k = new LZW(baseSourceRoute + "moby-1k.txt", baseWriteRoute + "lzw/" + "moby-1k-lzw.txt");
+            lzw_moby1k.Decode();
+            Console.WriteLine(s.Elapsed + "\n");
+            s.Reset();
+
+            Console.WriteLine("lzw moby-10k.txt");
+            s.Start();
+            LZW lzw_moby10k = new LZW(baseSourceRoute + "moby-10k.txt", baseWriteRoute + "lzw/" + "moby-10k-lzw.txt");
+            lzw_moby10k.Decode();
+            Console.WriteLine(s.Elapsed + "\n");
+            s.Reset();
+
+            Console.WriteLine("lzw moby-100k.txt");
+            s.Start();
+            LZW lzw_moby100k = new LZW(baseSourceRoute + "moby-100k.txt", baseWriteRoute + "lzw/" + "moby-100k-lzw.txt");
+            lzw_moby100k.Decode();
+            Console.WriteLine(s.Elapsed + "\n");
+            s.Reset();
+
+            Console.WriteLine("lzw moby-1m.txt");
+            s.Start();
+            LZW lzw_moby1m = new LZW(baseSourceRoute + "moby-1m.txt", baseWriteRoute + "lzw/" + "moby-1m-lzw.txt");
+            lzw_moby1m.Decode();
+            Console.WriteLine(s.Elapsed + "\n");
+            s.Reset();
+            Console.WriteLine("______________________________________\n");
+            Console.WriteLine("______________________________________\n");
+
+            Console.WriteLine("lzw random-100.txt");
+            Console.WriteLine("random-100.txt code table:");
+            s.Start();
+            LZW lzw_random100 = new LZW(baseSourceRoute + "random-100.txt", baseWriteRoute + "lzw/" + "random-100-lzw.txt");
+            lzw_random100.Decode();
+            Console.WriteLine(s.Elapsed + "\n");
+            s.Reset();
+
+            Console.WriteLine("lzw random-1k.txt");
+            s.Start();
+            LZW lzw_random1k = new LZW(baseSourceRoute + "random-1k.txt", baseWriteRoute + "lzw/" + "random-1k-lzw.txt");
+            lzw_random1k.Decode();
+            Console.WriteLine(s.Elapsed + "\n");
+            s.Reset();
+
+            Console.WriteLine("lzw random-10k.txt");
+            s.Start();
+            LZW lzw_random10k = new LZW(baseSourceRoute + "random-10k.txt", baseWriteRoute + "lzw/" + "random-10k-lzw.txt");
+            lzw_random10k.Decode();
+            Console.WriteLine(s.Elapsed + "\n");
+            s.Reset();
+
+            Console.WriteLine("lzw random-100k.txt");
+            s.Start();
+            LZW lzw_random100k = new LZW(baseSourceRoute + "random-100k.txt", baseWriteRoute + "lzw/" + "random-100k-lzw.txt");
+            lzw_random100k.Decode();
+            Console.WriteLine(s.Elapsed + "\n");
+            s.Reset();
+
+            Console.WriteLine("lzw random-1m.txt");
+            s.Start();
+            LZW lzw_random1m = new LZW(baseSourceRoute + "random-1m.txt", baseWriteRoute + "lzw/" + "random-1m-lzw.txt");
+            lzw_random1m.Decode();
+            Console.WriteLine(s.Elapsed + "\n");
+            s.Reset();
+            Console.WriteLine("______________________________________\n");
+            Console.WriteLine("______________________________________\n");
         }
     }
 }
